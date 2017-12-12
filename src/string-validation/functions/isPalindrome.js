71a98d967c1937de152364a51d0e56e90bbc16a5
Idrees <dargahidrees@gmail.com>
2017-12-12 22:41:40 +0530

    isPalindrome: remove non-alphanumeric characters

--- src/string-validation/functions/isPalindrome.js
+++ src/string-validation/functions/isPalindrome.js
@@ -2,7 +2,7 @@ const string_util = require('./util/isString');
 
 export default function isPalindrome(str) {
     string_util.isString(str);
-    const reverse = str.split("").reverse().join("");
+    const reverse = str.toLowerCase().replace(/[\W_]/g,'').split("").reverse().join("");
 
-    return str === reverse;
+    return str.toLowerCase().replace(/[\W_]/g,'') === reverse;
 };
