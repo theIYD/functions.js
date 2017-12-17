12664d58200714500cad1f5889ce30876c492ce3
Idrees <dargahidrees@gmail.com>
2017-12-17 13:33:07 +0530

    update contributing guide

--- CONTRIBUTING.md
+++ CONTRIBUTING.md
@@ -1,15 +1,17 @@
-# Contributing
+# Contributing ✏️
  To this project, you can contribute by sending pull requests for new functions that might solve some problem or reduce the complexity of a problem.
 
 ### Guide for new functions
  
 -  The function should be created in <code>src/{category}/functions/{filename.js}</code>. Add the function names to <code>category.js</code> and <code>allfunctions.js</code>.
 
-- While adding the function names to above mentioned files, make sure to maintain the alphabetical order.
+- While adding the function names to above mentioned files, make sure to maintain the **alphabetical order**.
 
-- The function should be explained above the function declaration itself with the help of multi-line comments. 
+- The function should be explained above the function declaration itself with the help of **JSDOC**. 
 
 - The code written should be preferably in **ES6** standard.
 
-- Be sure to test the function before making a pull request. 
+- Be sure to **test** the function before making a pull request. 
+
+- If you have a new category to be included, feel free to make a pull request. 
 
--- scripts/generator.js
+++ scripts/generator.js
@@ -39,18 +39,20 @@ const about = `\n## About\n The project is a continuation to a package previousl
 const license = `\n## License\n The project is licensed under <a href="https://github.com/theIYD/functions.js/blob/master/LICENSE">MIT</a>`;
 output = header + install + usage + func_generated_table + about + license;
 
-const contributing_guide_header = `# Contributing\n To this project, you can contribute by sending pull requests for new functions that might solve some problem or reduce the complexity of a problem.\n`;
+const contributing_guide_header = `# Contributing ✏️\n To this project, you can contribute by sending pull requests for new functions that might solve some problem or reduce the complexity of a problem.\n`;
 
 const contributing_guide_content = `\n### Guide for new functions\n 
 -  The function should be created in <code>src/{category}/functions/{filename.js}</code>. Add the function names to <code>category.js</code> and <code>allfunctions.js</code>.
 
-- While adding the function names to above mentioned files, make sure to maintain the alphabetical order.
+- While adding the function names to above mentioned files, make sure to maintain the **alphabetical order**.
 
-- The function should be explained above the function declaration itself with the help of multi-line comments. 
+- The function should be explained above the function declaration itself with the help of **JSDOC**. 
 
 - The code written should be preferably in **ES6** standard.
 
-- Be sure to test the function before making a pull request. 
+- Be sure to **test** the function before making a pull request. 
+
+- If you have a new category to be included, feel free to make a pull request. 
 
 `;
 
