5ffb340a3c783e1129d68105dd07dadb8e4fc11b
Idrees <dargahidrees@gmail.com>
2017-12-23 08:43:29 +0530

    integrate travis

---
+++ .travis/push.sh
@@ -0,0 +1,21 @@
+#!/bin/sh
+
+setup_git() {
+  git config --global user.email "travis@travis-ci.org"
+  git config --global user.name "Travis CI"
+}
+
+commit_website_files() {
+  git checkout master
+  git add .
+  git commit --message "Travis build: $TRAVIS_BUILD_NUMBER"
+}
+
+upload_files() {
+   echo "https://${GH_TOKEN}@github.com/theIYD/functions.js.git"
+   git push --force "https://${GH_TOKEN}@github.com/theIYD/functions.js.git" master > /dev/null 2>&1
+}
+
+setup_git
+commit_website_files
+upload_files
